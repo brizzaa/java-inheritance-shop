6175842f990f63b3a91d845119e5eadd40da20da
brizzaa <brizzantex@gmail.com>
2025-05-09 00:33:13 +0200

    feat: commit

--- src/com/inheritance/shop/App.java
+++ src/com/inheritance/shop/App.java
@@ -2,9 +2,26 @@ package com.inheritance.shop;
 
 public class App {
     public static void main(String[] args) {
+        
         Televisione uno = new Televisione("logitech 21", "logitech", 420, 210, false);
         Televisione due = new Televisione("logitech smart", "logitech", 40, 320, true);
-        Smartphone iphion = new Smartphone("IPHONE Z", "appl", 299, 130987412, 28);
+        Smartphone iphoneZ = new Smartphone("IPHONE Z", "appl", 299, 130987412, 28);
+        Cuffie logitechG402 = new Cuffie("cuffiozze LOGITECH", "logitech", 90, "Blu cielo", true);
+
+        System.out.println(iphoneZ.getCodiceProdotto());
+        System.out.println(due.getCodiceProdotto());
+        System.out.println(uno.getCodiceProdotto());
+
+
+        System.out.println(logitechG402.getBluetoothProdotto());
+
+        System.out.println(logitechG402.getCodiceProdotto());
+        logitechG402.setCodiceProdotto(30839);
+        System.out.println(logitechG402.getCodiceProdotto());
+   
+        
+
+
 
     }
 }
--- src/com/inheritance/shop/Cuffie.java
+++ src/com/inheritance/shop/Cuffie.java
@@ -1,1 +1,41 @@
 package com.inheritance.shop;
+// colore e se bluetooth
+
+public class Cuffie extends Prodotto{
+
+    private String coloreProdotto;
+    private boolean bluetoothProdotto;
+
+    public Cuffie(String nome, String marca, double prezzo, int iva, String colore, boolean bluetooth){
+
+        super(nome, marca, prezzo, iva);
+        this.coloreProdotto = colore;
+        this.bluetoothProdotto = bluetooth;
+
+    }
+
+    public Cuffie(String nome, String marca, double prezzo, String colore, boolean bluetooth){
+
+        super(nome, marca, prezzo);
+        this.coloreProdotto = colore;
+        this.bluetoothProdotto = bluetooth;
+        
+    }
+
+    public void setColoreProdotto(String colore){
+        this.coloreProdotto = colore;
+    }
+
+    public void setBluetoothProdotto(boolean bluetooth){
+        this.bluetoothProdotto = bluetooth;
+    }
+
+    public String getColoreProdotto(){
+        return this.coloreProdotto;
+    }
+
+    public boolean getBluetoothProdotto(){
+        return this.bluetoothProdotto;
+    }
+
+}
